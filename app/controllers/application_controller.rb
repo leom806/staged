9f0d6f6200e7292e897623d11d67e42c5680d434
Leonardo Momente <leom806@gmail.com>
2023-09-25 15:02:25 -0300

    Add a bit of translations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -6,5 +7,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
   end
 end
